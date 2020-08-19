d2ae74cce359bbd3693d3a76804acf82affd7ccd
4423 <hanamiya4423@gmail.com>
2020-08-19 21:05:03 +0900

    add: google analytics

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueAnalytics from 'vue-analytics'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
+const routes = [
+{
+  path: '/',
+  name: 'Home',
+  component: Home
+},
 ]
 
 const router = new VueRouter({
   mode: 'history',
   routes: routes,
+})
+
+Vue.use(VueAnalytics, {
+  id: 'UA-57215541-10',
+  router,
 })
 
 export default router
